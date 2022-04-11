f919910622b2364b6b773697ceda0711163f4186
hakutai <hiro@hakutai.org>
2022-04-11 16:00:48 +0900

    各PALセンサー対応へ

--- twelitePalRead.py
+++ twelitePalRead.py
@@ -8,6 +8,7 @@
 # usage : twelitePalRead DEVICE-NAME
 #
 #
+#
 ##########################################################################
 
 from serial import *
@@ -21,9 +22,14 @@ readThread = None	# 読み出しスレッド
 isExit = False		# プログラム終了フラグ
 
 # センサーパルの種類
-#palTyep = {"81" : "OpenClose", "82" : "Ambient", "83" : "Motion", "84" : "Notice" }
-palTyep   = { '81' : '開閉', '82' : '環境', '83' : '動作', '84' : '通知' }
-senseType = { '00' : '磁気', '01' : '温度', '02' : '湿度', '03' : '照度', '04' : '加速度', '05' : 'イベント', '30' : 'ADC' }
+palIdList   = { '81' : '開閉', '82' : '環境', '83' : '動作', '84' : '通知' }
+sensorList = { 	'00' : { 'name':'磁気',    'unit':'',   'datatype':'00' },
+				'01' : { 'name':'温度',    'unit':'℃', 'datatype':'05' },
+				'02' : { 'name':'湿度',    'unit':'％', 'datatype':'01' },
+				'03' : { 'name':'照度',    'unit':'lux','datatype':'02' },
+				'04' : { 'name':'加速度',  'unit':'mg', 'datatype':'15' },
+				'05' : { 'name':'イベント','unit':'',   'datatype':'12' },
+				'30' : { 'name':'ADC',     'unit':'mV', 'datatype':'11' }}
 
 #
 
@@ -37,14 +43,50 @@ def printPayload(l):
 
 	dt_now = datetime.datetime.now()
 
+	sizeData = l[14]			# 受信データ数
+
 	print("---DataNo 0x%04x" % (l[5] << 8 | l[6]) , dt_now.strftime('%H:%M'))
 	print(" Device ID : %02x " % l[11])
 	print(" LQI       : %d / %.2f [dbm]" % (l[4],(7 * l[4] - 1970) / 20.))
-	print(" ADC       : %dmV" % (l[19] << 8 | l[20]))
-	print(" ADC1      : %dmV" % (l[25] << 8 | l[26]))
-	print(" TemperaturE : %.2fC" % ((l[31] << 8 | l[32]) / 100.) , end=" /")
-	print(" Humidity : %.2f%%" % ((l[37] << 8 | l[38]) / 100.) , end=" /")
-	print(" Illuminance : %dlux" % (l[43] << 24 | l[44] << 16 | l[45] << 8 | l[46]))
+
+	index = 15
+	for kData in range(sizeData):
+		sensorID = format(l[index + 1],'02x')
+		sensor = sensorList[sensorID]
+		sensorName  = sensor['name']
+		sensorValue = None
+		valueUnit   = sensor['unit']
+
+		if sensorID == '00':i				# 磁気
+			sensorValue = l[index + 4]
+		elif sensorID == '01':				# 温度
+			sensorValue = (l[index + 4] << 8 | l[index + 5]) / 100
+		elif sensorID == '02':				# 湿度
+			sensorValue = (l[index + 4] << 8 | l[index + 5]) / 100
+		elif sensorID == '03':				# 照度
+			sensorValue = l[index + 4] << 24 | l[index + 5] << 16 \
+						| l[index + 6] <<  8 | l[index + 7]
+		elif sensorID == '04':				# 加速度
+			sensorValue = "X:"+str(l[index + 4]<<8 | l[index + 5])+"mg,"  \
+						+ "Y:"+str(l[index + 6]<<8 | l[index + 7])+"mg," 
+#						+ "Z:"+str(l[index + 8] << 8 | l[index + 9]) + "mg"
+		elif sensorID == '05':				# イベント
+			sensorValue = l[index + 4]
+		elif sensorID == '30':				# ADC
+			sensorValue = l[index + 4] << 8 | l[index + 5]
+			if l[index + 2] == 8:
+				sensorName = '電源電圧'
+			elif l[index + 2] == 1:
+				sensorName = 'ADC1'
+
+		print(sensorName,end=":")
+		print(sensorValue,end="")
+		print(valueUnit,end=" /")
+
+		index += 4 + l[index + 3]
+
+	print(" ")
+
 
 	return True
 
@@ -82,6 +124,7 @@ def readThread():
 
 			chksum = sum(lst) & 0xff
 			lst = lst[0:len(lst)-1]
+
 			if chksum == 0:
 				printPayload(lst)
 			else:
